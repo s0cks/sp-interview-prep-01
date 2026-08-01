39b9a5b00d733119454487b5ad765539981d821a
tazz <tasman.vose@gmail.com>
2026-08-01 17:31:43 -0400

    feat(service): ✨ add GET /sensors/:id endpoint
    Add a service endpoint for http GET /sensors/:id that returns all sensor data
    
    #4, closes #6

--- internal/sensor/service.go
+++ internal/sensor/service.go
@@ -12,6 +12,14 @@ var (
 	buffers sync.Map
 )
 
+type PostDataRequest struct {
+	Data float64 `json:"data"`
+}
+
+type Response struct {
+	Data any `json:"data"`
+}
+
 func append(ctx context.Context, sid string, item float64) {
 	actual, _ := buffers.LoadOrStore(sid, NewSensorBuffer(10))
 	sb, ok := actual.(*SensorBuffer)
@@ -23,8 +31,23 @@ func append(ctx context.Context, sid string, item float64) {
 	fmt.Printf("wrote %f for %v\n", item, sid)
 }
 
-type PostDataRequest struct {
-	Data float64 `json:"data"`
+func getAll(ctx context.Context, sid string, resultsCh chan []float64) {
+	actual, _ := buffers.LoadOrStore(sid, NewSensorBuffer(10))
+	sb, ok := actual.(*SensorBuffer)
+	if !ok {
+		return
+	}
+
+	sb.lock.RLock()
+	defer sb.lock.RUnlock()
+
+	if sb.length == 0 {
+		resultsCh <- make([]float64, 0)
+		return
+	}
+
+	var results []float64 = sb.data[0:sb.length]
+	resultsCh <- results
 }
 
 func HandlePostData(w http.ResponseWriter, r *http.Request) {
@@ -40,7 +63,35 @@ func HandlePostData(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
-	w.Write([]byte(``))
+}
+
+func HandleGetAllData(w http.ResponseWriter, r *http.Request) {
+	sid := r.PathValue("id")
+	ctx := r.Context()
+	resultCh := make(chan []float64, 1)
+
+	go getAll(ctx, sid, resultCh)
+
+	w.Header().Set("Content-Type", "application/json")
+	var res Response
+	select {
+	case <-ctx.Done():
+		return
+	case results := <-resultCh:
+		res = Response{
+			Data: results,
+		}
+
+		if len(results) == 0 {
+			w.WriteHeader(http.StatusNoContent)
+		} else {
+			w.WriteHeader(http.StatusOK)
+			if err := json.NewEncoder(w).Encode(res); err != nil {
+				http.Error(w, "failed to encode json", http.StatusInternalServerError)
+				return
+			}
+		}
+	}
 }
 
 func ListenAndServe(port int) {
@@ -48,6 +99,7 @@ func ListenAndServe(port int) {
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /sensors/{id}", HandlePostData)
+	router.HandleFunc("GET /sensors/{id}", HandleGetAllData)
 
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), router); err != nil {
 		fmt.Printf("failed to start service: %v\n", err)
