28e204f6565dfca7f8137c9b3a1c4e3c87b6878c
tazz <tasman.vose@gmail.com>
2026-08-01 18:06:00 -0400

    feat(service): ✨ add GET /sensors/:id/:metric endpoint
    #4, closes #7

--- internal/sensor/service.go
+++ internal/sensor/service.go
@@ -50,6 +50,35 @@ func getAll(ctx context.Context, sid string, resultsCh chan []float64) {
 	resultsCh <- results
 }
 
+func getMetric(ctx context.Context, sid string, metric string, resultsCh chan any) {
+	actual, _ := buffers.LoadOrStore(sid, NewSensorBuffer(10))
+	sb, ok := actual.(*SensorBuffer)
+	if !ok {
+		resultsCh <- fmt.Errorf("invalid buffer type")
+		return
+	}
+
+	sb.lock.RLock()
+	defer sb.lock.RUnlock()
+	if sb.length == 0 {
+		resultsCh <- nil
+		return
+	}
+
+	results, err := sb.CalcMetrics()
+	if err != nil {
+		resultsCh <- err
+		return
+	}
+
+	switch metric {
+	case "avg":
+		resultsCh <- results.avg
+	case "sum":
+		resultsCh <- results.sum
+	}
+}
+
 func HandlePostData(w http.ResponseWriter, r *http.Request) {
 	sid := r.PathValue("id")
 
@@ -94,12 +123,81 @@ func HandleGetAllData(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type ErrorResponse struct {
+	Error error
+}
+
+type MessageResponse struct {
+	Message string `json:"message"`
+}
+
+func HandleGetMetricData(w http.ResponseWriter, r *http.Request) {
+	sid := r.PathValue("id")
+	metric := r.PathValue("metric")
+	ctx := r.Context()
+	resultCh := make(chan any)
+
+	fmt.Printf("getting %s metric for sensor %s\n", metric, sid)
+
+	w.Header().Set("Content-Type", "application/json")
+	switch metric {
+	case "avg", "sum":
+		go getMetric(ctx, sid, metric, resultCh)
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		var payload MessageResponse = MessageResponse{
+			Message: fmt.Sprintf("invalid metric: %s", metric),
+		}
+
+		if err := json.NewEncoder(w).Encode(payload); err != nil {
+			http.Error(w, "failed to encode json", http.StatusInternalServerError)
+			return
+		}
+		return
+	}
+
+	var res Response
+	select {
+	case <-ctx.Done():
+		return
+	case val := <-resultCh:
+		switch v := val.(type) {
+		case nil:
+			w.WriteHeader(http.StatusNoContent)
+			return
+		case error:
+			w.WriteHeader(http.StatusInternalServerError)
+			payload := struct {
+				Error string `json:"message"`
+			}{
+				Error: v.Error(),
+			}
+
+			if err := json.NewEncoder(w).Encode(payload); err != nil {
+				http.Error(w, "failed to encode json", http.StatusInternalServerError)
+				return
+			}
+		default:
+			res = Response{
+				Data: v,
+			}
+
+			w.WriteHeader(http.StatusOK)
+			if err := json.NewEncoder(w).Encode(res); err != nil {
+				http.Error(w, "failed to encode json", http.StatusInternalServerError)
+				return
+			}
+		}
+	}
+}
+
 func ListenAndServe(port int) {
 	fmt.Printf("service is listening on http://localhost:%d\n", port)
 	router := http.NewServeMux()
 
 	router.HandleFunc("POST /sensors/{id}", HandlePostData)
 	router.HandleFunc("GET /sensors/{id}", HandleGetAllData)
+	router.HandleFunc("GET /sensors/{id}/{metric}", HandleGetMetricData)
 
 	if err := http.ListenAndServe(fmt.Sprintf(":%d", port), router); err != nil {
 		fmt.Printf("failed to start service: %v\n", err)
